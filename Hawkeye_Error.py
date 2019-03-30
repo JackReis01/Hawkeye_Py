65f727095e9af2eed14c91738e0774cc9dee2731
JackReis01 <jackreis2019@gmail.com>
2019-03-29 23:24:29 -0500

    add

--- Hawkeye_Error.py
+++ Hawkeye_Error.py
@@ -22,3 +22,12 @@ while True:
      break
 userInput=str(userInput)
 print("Your number is " + userInput + ".")
+
+mylist = []
+n = int(input("Enter size of the list:\n"))
+
+for i in range(0, n):
+    temp = int(input("Enter number to append:\n"))
+    mylist.append(temp)
+
+print(mylist)
