83fa6de7366099ef19df9d6c939e2ce5d2b11e68
JackReis01 <jackreis2019@gmail.com>
2019-03-29 23:23:51 -0500

    add

--- Hawkeye_Error.py
+++ Hawkeye_Error.py
@@ -9,3 +9,16 @@ ladders=[9,4,8,2,6,1,7,3,19,2]
 ladders[0]= 0
 ladders.sort(reverse=True)
 print(ladders[0])
+
+userInput = 0
+while True:
+  try:
+     userInput = int(input("Enter something: "))
+  except ValueError:
+     print("Not an integer!")
+     continue
+  else:
+     print("Yes an integer!")
+     break
+userInput=str(userInput)
+print("Your number is " + userInput + ".")
