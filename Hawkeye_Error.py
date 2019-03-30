8a5fdc6fdaf07d58579549712936ca9a3d15115b
JackReis01 <jackreis2019@gmail.com>
2019-03-29 23:22:59 -0500

    Add

--- Hawkeye_Error.py
+++ Hawkeye_Error.py
@@ -4,3 +4,8 @@ if user == "":
         user = input("Input an answer.")
 print("Your day was " + user + ".")
 print(user)
+
+ladders=[9,4,8,2,6,1,7,3,19,2]
+ladders[0]= 0
+ladders.sort(reverse=True)
+print(ladders[0])
